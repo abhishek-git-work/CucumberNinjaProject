9952d00879ba2e267229d6a6db5c0965f66a121f
Admin <Admin@DESKTOP-7NNF0FV>
2023-07-13 14:13:27 +0530

    13-7-23....

--- src/test/java/hooks/MyHooks.java
+++ src/test/java/hooks/MyHooks.java
@@ -1,3 +1,12 @@
+//@Before and @After hooks will run before and after every scenario
+//@BeforeStep and @AfterStep hooks will run before and after every steps in scenario
+//@BeforeAll and @AfterAll hooks will run once before any scenario is run and after all scenario have been run.
+//there can be many same type of hooks in project and we can give the order to execute that same hooks
+//order for @Before hooks is ascending type i.e.@Before(order=0,1,...) and for @After hooks is descending type i.e. @After(order=3,2,1..)
+//we can also give tags to hooks, so it will run only for that specific tag scenarios 
+//user value attribute for taged hooks i.e.@Before(value="@search")
+//we can use multiple tags in one hook i.e. @Before(value="@search", order=1)
+
 package hooks;
 
 import java.io.IOException;
